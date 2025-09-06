3a2b5e1d3fd16e4822ea5e96031212a480f5ea4d
Jas-39 Gripen lover <beezet29@gmail.com>
2025-09-06 20:17:56 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "ROFL" : "tanggapan terhadap lelucon",
+            "SHEESH" : "sedikit ketidaksetujuan",
+            "CREEPY" : "menakutkan, tidak menyenangkan",
+            "AGGRO" : "untuk menjadi agresif/marah",
+            }
+
+print("Welcome To Dictionary 🙌")
+for i in range(5):
+    word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ").upper()
+    if word in meme_dict.keys():
+        print('Makna dari kata ', word, "adalah", meme_dict[word])
+    else:
+        print("Kata tidak ditemukan")
